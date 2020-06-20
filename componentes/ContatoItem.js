11572c51e256d0074fed9870fbcb3e8b608f2e26
Daniel-Peres <dsperes@gmail.com>
2020-06-20 15:46:56 -0300

    Placeholder no ContatoItem

--- componentes/ContatoItem.js
+++ componentes/ContatoItem.js
@@ -5,6 +5,7 @@ const ContatoItem = (props) => {
     return (
         <TouchableOpacity onLongPress={() => props.onDelete(props.contato.key)}>
             <View style={estilos.itemNaLista}>
+                <Text style={estilos.textoItem}>[ toque 2 segundos para excluir ]</Text>
                 <Text>Nome: {props.contato.nome}</Text>
                 <Text>Telefone: {props.contato.telefone}</Text>
             </View>
@@ -23,6 +24,10 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         width: '80%',
         alignSelf: 'center'
+    },
+    textoItem: {
+        color: '#c9c9c9',
+        fontSize: 10
     }
 });
 
